148c5ea198127983835652ca25648e4abe8c499b
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-06-17 13:55:46 +0200

    make it accessable from CLI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="docgenerator",
-    version="0.0.1",
+    version="0.0.2",
 
     # The packages that constitute your project.
     # For my project, I have only one - "pydash".
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(),
     
     entry_points = {
-        'console_scripts': ['docgenerator=docgenerator.command_line:main'],
+        'console_scripts': ['docgenerator=docgenerator.app:main'],
     },
 
     # The description that will be shown on PyPI.
