eee8c67d2cd3b65b76de6647726091397f1ef5b5
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-07-18 14:04:22 +0200

    Update setup.py
    Signed-off-by: Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
 
 setup(
     name="docgenerator",
-    version="1.0.1",
+    version="1.0.2",
 
     # The packages that constitute your project.
     # For my project, I have only one - "pydash".
